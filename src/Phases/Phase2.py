46fec072ade6aaf75575228f9d89b4bb9fe79368
Pedraum1 <pedro.paulo60@aluno.ifce.edu.br>
2026-06-12 10:14:31 -0300

    Adicionando role Assassino

--- src/Phases/Phase2.py
+++ src/Phases/Phase2.py
@@ -2,6 +2,7 @@ from src.Engine.Phase import Phase
 from src.Character.Character import Character
 from src.Character.Role.Warrior import Warrior
 from src.Character.Role.Mage import Mage
+from src.Character.Role.Assassin import Assassin
 
 from src.Engine.Combat import Combat
 
@@ -13,4 +14,4 @@ class Phase2(Phase):
     def run(self, player: Character):
         print("Fase 2 rodando")
 
-        Combat().run(player, [Character(Mage(), "Gandalf")])
+        Combat().run(player, [Character(Assassin(), "Zed"), Character(Mage(), "Ryze")])
